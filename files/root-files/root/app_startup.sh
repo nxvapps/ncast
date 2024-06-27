7784925f67bfd805585bd13cf69005cb94b29f0c
ebenner <ebenner@vultr.com>
2024-06-27 15:45:10 -0400

    Fix obs

--- files/root-files/root/app_startup.sh
+++ files/root-files/root/app_startup.sh
@@ -6,12 +6,12 @@ export OBS_PORT="${OBS_PORT:-4455}"
 
 if [ -z "$(ls -A /data)" ]; then
    echo "Copying default files and linking fs" | tee /var/log/ncast.log
-   cp -rf /root/.config/obs-default /data/obs
-   ln -s /data/obs /root/.config/obs
+   cp -rf /root/.config/obs-default /data/obs-studio
+   ln -s /data/obs-studio /root/.config/obs-studio
 fi
 
 echo "Settings OBS Server Port: ${OBS_PORT}" | tee /var/log/ncast.log
-sed -i -e "s/ServerPort=.*+/ServerPort=${OBS_PORT}/g" /root/.config/obs/obs-studio/global.ini
+sed -i -e "s/ServerPort=.*+/ServerPort=${OBS_PORT}/g" /root/.config/obs-studio/global.ini
 
 echo "Setting OBS Password to APP_PASSWORD" | tee /var/log/ncast.log
-sed -i -e "s/ServerPassword=.*+/ServerPassword=${APP_PASSWORD}/g" /root/.config/obs/obs-studio/global.ini
+sed -i -e "s/ServerPassword=.*+/ServerPassword=${APP_PASSWORD}/g" /root/.config/obs-studio/global.ini
